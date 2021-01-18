7468f43ae3eee1f1a20cdf7292a01db26d298d65
潘祎 <920524584@qq.com>
2021-01-18 21:41:09 +0800

    fkdf

--- src/main/java/github_test.java
+++ src/main/java/github_test.java
@@ -1,2 +1,6 @@
 public class github_test {
+    public static  void main(String[] args){
+        System.out.println("zzzzzzzzzzzzzzzzzz");
+        System.out.println("ssssssssssssssssss");
+    }
 }
